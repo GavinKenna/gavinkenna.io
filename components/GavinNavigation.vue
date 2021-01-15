a6e4cc2c7be44a256b3c23e40b7f538d305d992b
Gavin Kenna <thegavinkenna@gmail.com>
2021-01-15 20:06:16 +0000

    Adding git commit information

--- components/GavinNavigation.vue
+++ components/GavinNavigation.vue
@@ -8,28 +8,47 @@
         class="navbar-toggler"
         data-target="#options"
         data-toggle="collapse"
-        type="button">
-        <span class="navbar-toggler-icon"></span>
+        type="button"
+      >
+        <span class="navbar-toggler-icon" />
       </button>
       <a class="navbar-brand mb-0 h1" href="#">GavinKenna.io</a>
 
-      <div class="collapse navbar-collapse" id="options">
+      <div id="options" class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item">
-            <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+            <NuxtLink class="nav-link" to="/">
+              Home
+            </NuxtLink>
           </li>
           <li class="nav-item">
-            <NuxtLink class="nav-link" to="/blog">Blog</NuxtLink>
+            <NuxtLink class="nav-link" to="/blog">
+              Blog
+            </NuxtLink>
           </li>
           <li class="nav-item">
-            <NuxtLink class="nav-link" to="/about">About</NuxtLink>
+            <NuxtLink class="nav-link" to="/about">
+              About
+            </NuxtLink>
           </li>
-
         </ul>
         <li style="alignment: right">
-          <p class="navbar-brand mb-0 h1">v1.0</p>
+          <fa style="color: white" :icon="['fab','github']" />
+          <a class="navbar-brand mb-0 h1" :href="`https://github.com/GavinKenna/GavinKenna.io]/commit/${commitSha}`">
+            Current Code Version # {{ commitSha }}
+          </a>
         </li>
       </div>
     </nav>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      commitSha: process.env.GIT_COMMIT
+    }
+  }
+}
+</script>
